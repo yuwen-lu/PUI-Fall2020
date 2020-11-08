3baae7d04e5257057b815dec6af21f228dc93be7
Yuwen <wayne.lu1998@gmail.com>
2020-11-08 16:27:21 -0500

    Create folder for assignment 6b

---
+++ assignment_6b/js/cart.js
@@ -0,0 +1,170 @@
+// get the necessary html elements for later shopping cart item display
+let cartContainer = document.getElementsByClassName("shopping-cart")[0];
+let cartEmptyMessage = document.getElementsByClassName("cart-empty-message")[0];
+let checkoutButton = document.getElementsByClassName("shopping-cart-checkout-button")[0];
+let totalAmount = document.getElementsByClassName("total-amount")[0];
+
+// variable to calculate the total amount
+let totalAmountNum = 0;
+
+// function to create a new shopping cart item
+function createNewItem(name, quantity, glazing) {
+
+    // create a hr first
+    let line = document.createElement("hr");
+
+    // the basic structure: whole container, left container, right container
+    let itemContainer = document.createElement("div");
+    itemContainer.className = "shopping-cart-item";
+    let itemLeftContainer = document.createElement("div");
+    itemLeftContainer.className = "shopping-cart-item-left";
+    let itemRightContainer = document.createElement("div");
+    itemRightContainer.className = "shopping-cart-item-right";
+    cartContainer.insertBefore(line, cartEmptyMessage);
+    cartContainer.insertBefore(itemContainer, cartEmptyMessage);
+    itemContainer.appendChild(itemLeftContainer);
+    itemContainer.appendChild(itemRightContainer);
+
+    // content in the left container: image, name
+    let itemImage = document.createElement("div");
+    itemImage.classList.add("shopping-cart-item-img");
+    let itemName = document.createElement("div");
+    itemName.classList.add("underlined");
+    itemLeftContainer.appendChild(itemImage);
+    itemLeftContainer.appendChild(itemName);
+
+    let text = document.createTextNode(name);
+    itemName.appendChild(text);
+    
+    // add item-specific image classes to itemImage
+    
+    switch(name) {
+        case "Original":
+            itemImage.classList.add("shopping-cart-original-image");
+            break;
+        case "Blackberry":
+            itemImage.classList.add("shopping-cart-blackberry-image");
+            break;
+        case "Walnut":
+            itemImage.classList.add("shopping-cart-walnut-image");
+            break;
+        case "Original Gluten Free":
+            itemImage.classList.add("shopping-cart-original-gluten-free-image");
+            break;
+        case "Pumpkin Spice":
+            itemImage.classList.add("shopping-cart-pumpkin-spice-image");
+            break;
+        case "Caramel Peacan":
+            itemImage.classList.add("shopping-cart-caramel-peacan-image");
+            break;
+    
+        default:
+            itemImage.classList.add("shopping-cart-original-image");
+            break;
+    }
+
+    // content in the right container: two dropdowns, price, delete button
+
+    // quantity dropdown
+    let quantityDropdown = document.createElement("select");
+    quantityDropdown.classList.add("cart-dropdown-appearance");
+    itemContainer.appendChild(quantityDropdown);
+    // array of quantity dropdown values
+    let quantityValues = [1, 3, 6, 12];
+    // array of quantity dropdown texts
+    let quantityTexts = ["1 roll", "3 rolls", "6 rolls", "12 rolls"];
+    for(i in quantityValues) {
+        let option = document.createElement("option");
+        option.value = quantityValues[i];
+        option.text = quantityTexts[i];
+        // make sure to display users' previous selection of quantity
+        if (quantity === option.value)
+            option.selected = true;
+        
+            quantityDropdown.appendChild(option);
+    }
+
+    // glazing dropdown
+    let glazingDropdown = document.createElement("select");
+    glazingDropdown.classList.add("cart-dropdown-appearance");
+    itemContainer.appendChild(glazingDropdown);
+    // array of glazing dropdown values
+    let glazingValues = ["None", "Sugar Milk", "Vanilla Milk", "Double Chocolate"];
+    // array of glazing dropdown texts
+    let glazingTexts = ["none", "sugar milk", "vanilla milk", "double chocolate"];
+    for(i in glazingValues) {
+        let option = document.createElement("option");
+        option.value = glazingValues[i];
+        option.textContent = glazingTexts[i];
+        // make sure to display users' previous selection of glazing
+        if (glazing === option.value)
+            option.selected = true;
+        
+            glazingDropdown.appendChild(option);
+    }
+
+    // price
+    let priceContainer = document.createElement("div");
+    priceContainer.classList.add("shopping-cart-item-price");
+    let moneySymbol = document.createElement("span");
+    moneySymbol.className = "golden-color";
+    moneySymbol.textContent = "$";
+    // dynamically loading price based on user's choice
+    let priceNum = Number(quantity * 3.00);
+    // update the total amount on page
+    totalAmountNum += priceNum;
+    let priceText = document.createTextNode(" " + String(priceNum.toFixed(2)));
+    priceContainer.appendChild(moneySymbol);
+    priceContainer.appendChild(priceText);
+    itemContainer.appendChild(priceContainer);
+
+    // delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "delete-button golden-icon";
+    itemContainer.appendChild(priceContainer);
+
+    // append them all to the right container
+    itemRightContainer.appendChild(quantityDropdown);
+    itemRightContainer.appendChild(glazingDropdown);
+    itemRightContainer.appendChild(priceContainer);
+    itemRightContainer.appendChild(deleteButton);
+}
+
+// retrieve localStorage for display items
+let items = JSON.parse(localStorage.getItem("productItems"));
+
+// if the shopping cart is not empty
+if(items) {
+    let keys = Object.keys(items);
+    for(index in keys) {
+        let key = keys[index];
+        let [name, glazing] = key.split(",");
+        // Tweak the format of name a bit
+        nameList = name.split("-");
+        for (index in nameList) {
+            nameList[index] = nameList[index].charAt(0).toUpperCase() + nameList[index].slice(1);
+        }
+        name = nameList.join(" ");
+
+        let quantity = items[key];
+        createNewItem(name, quantity, glazing);
+    }
+
+    // calculate the total amount
+    let amountNode = document.createTextNode(String(totalAmountNum.toFixed(2)));
+    totalAmount.appendChild(amountNode);
+    totalAmount.style.display = "inline-block";
+    checkoutButton.style.display = "inline-block";
+}
+
+// if the cart is empty
+else {
+    document.getElementsByClassName("cart-empty-message")[0].style.display = "block";
+}
+
+// createNewItem("Original", 1, "Sugar Milk");
+
+
+checkoutButton.addEventListener('click', () => {
+    alert("Sorry, will implement this soon!");
+})
---
+++ assignment_6b/js/product-detail.js
@@ -0,0 +1,69 @@
+// // product item definition: will be used to store user added item into local storage
+// function RollItem(name, rollQuantity, rollGlazing) {
+//     this.name = name;
+//     this.rollQuantity = rollQuantity;
+//     this.rollGlazing = rollGlazing;
+// }
+
+
+// retrieve the rollName variable in localStorage, 
+// set by clicking on certain roll item in product-browsing.js
+
+let rollName = localStorage.getItem("rollName");
+let productDetailElementIdName = "product-detail-" + rollName;
+document.getElementById(productDetailElementIdName).style.display = "flex";
+
+// dynamically display user selection info on image, product detail page
+
+// get properties user selected for the product
+let quantitySelection = document.getElementById("quantity-dropdown-" + rollName);
+let quantity = "";
+let glazingSelection = document.getElementById("glazing-dropdown-" + rollName);
+let glazing = "";
+
+// get elements on the html page to display the selected properties
+let quantityDisplay = document.getElementById("quantity-display-" + rollName);
+let glazingDisplayText = document.getElementById("glazing-display-text-" + rollName);
+
+// add event listeners to these html elemenets so the displays update when selections change
+quantitySelection.addEventListener('change', () => {
+    quantity = quantitySelection.options[quantitySelection.selectedIndex].text.split(" ")[0]
+    quantityDisplay.innerText = quantity;
+    quantityDisplay.parentElement.style.display = "inline-block";
+});
+
+glazingSelection.addEventListener('change', () => {
+    glazing = glazingSelection.options[glazingSelection.selectedIndex].value;
+    glazingDisplayText.innerText = glazing;
+    glazingDisplayText.parentElement.style.display = "inline-block";
+})
+
+// store the user selections as shopping item objects into localStorage,
+// retrieved in cart.js and display in cart.html
+
+let addButton = document.getElementById(productDetailElementIdName).children[1].lastElementChild.lastElementChild;
+addButton.addEventListener('click', () => {
+    // firstly retrieve what's already in there
+    let currentItems = JSON.parse(localStorage.getItem("productItems"));
+
+    console.log("currentItems: ", currentItems);
+    
+    // then add the new item in to the current items object
+    if(currentItems) {
+        currentItems[rollName + "," + glazing] = quantity;
+    }
+    else {
+        currentItems = {};
+        currentItems[rollName + "," + glazing] = quantity;
+    }
+    console.log("new currentItems: ", currentItems);
+
+    localStorage.setItem("productItems", JSON.stringify(currentItems));
+
+    console.log(currentItems);
+    window.location.href = "./cart.html";
+})
+
+
+
+
