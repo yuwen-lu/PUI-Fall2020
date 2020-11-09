0e02bee5988a73e04d8a7578b6c5ca51a555171a
Yuwen <wayne.lu1998@gmail.com>
2020-11-08 20:26:26 -0500

    Resolve corner case of cart being empty

--- assignment_6b/js/cart-item.js
+++ assignment_6b/js/cart-item.js
@@ -1,10 +1,14 @@
 let cartItemAmount = document.getElementsByClassName("cart-num")[0];
 
 // get the localStorage content number to update cart number
-let prevProdItems =  localStorage.getItem("productItems") || {};
-let storageItems = JSON.parse(prevProdItems);
-let count = Object.keys(storageItems).length;
+let prevProdItems =  localStorage.getItem("productItems");
 
-if(count > 0) {
-    cartItemAmount.textContent = count;
+// if the localStorage item is not empty
+if(prevProdItems != null) {
+    let storageItems = JSON.parse(prevProdItems);
+    let count = Object.keys(storageItems).length;
+
+    if(count > 0) {
+        cartItemAmount.textContent = count;
+    }
 }
--- assignment_6b/js/cart.js
+++ assignment_6b/js/cart.js
@@ -8,162 +8,171 @@ let totalAmount = document.getElementsByClassName("total-amount")[0];
 let totalAmountNum = 0;
 
 // retrieve localStorage for display items
-let prevItems = localStorage.getItem("productItems") || {};
-let items = JSON.parse(prevItems);
+let prevItems = localStorage.getItem("productItems");
 
 // if the shopping cart is not empty
-if(Object.keys(items).length != 0) {
-    console.log(items);
-    console.log(items.length);
-    let keys = Object.keys(items);
-    for(let index = 0; index < keys.length; index++) {
-        let key = keys[index];
-        let [name, glazing] = key.split(",");
-        // Tweak the format of name a bit
-        nameList = name.split("-");
-        for (let i = 0; i < nameList.length; i++) {
-            nameList[i] = nameList[i].charAt(0).toUpperCase() + nameList[i].slice(1);
-        }
-        name = nameList.join(" ");
+if(prevItems != null) {
+    let items = JSON.parse(prevItems);
+    if(Object.keys(items).length != 0) {
+        console.log(items);
+        console.log(items.length);
+        let keys = Object.keys(items);
+        for(let index = 0; index < keys.length; index++) {
+            let key = keys[index];
+            let [name, glazing] = key.split(",");
+            // Tweak the format of name a bit
+            nameList = name.split("-");
+            for (let i = 0; i < nameList.length; i++) {
+                nameList[i] = nameList[i].charAt(0).toUpperCase() + nameList[i].slice(1);
+            }
+            name = nameList.join(" ");
 
-        let quantity = items[key];
+            let quantity = items[key];
 
-        // create new element based on the name, quantity, and glazing
+            // create new element based on the name, quantity, and glazing
 
-        // create a hr first
-        let line = document.createElement("hr");
+            // create a hr first
+            let line = document.createElement("hr");
 
-        // the basic structure: whole container, left container, right container
-        let itemContainer = document.createElement("div");
-        itemContainer.className = "shopping-cart-item";
-        let itemLeftContainer = document.createElement("div");
-        itemLeftContainer.className = "shopping-cart-item-left";
-        let itemRightContainer = document.createElement("div");
-        itemRightContainer.className = "shopping-cart-item-right";
-        cartContainer.insertBefore(line, cartEmptyMessage);
-        cartContainer.insertBefore(itemContainer, cartEmptyMessage);
-        itemContainer.appendChild(itemLeftContainer);
-        itemContainer.appendChild(itemRightContainer);
+            // the basic structure: whole container, left container, right container
+            let itemContainer = document.createElement("div");
+            itemContainer.className = "shopping-cart-item";
+            let itemLeftContainer = document.createElement("div");
+            itemLeftContainer.className = "shopping-cart-item-left";
+            let itemRightContainer = document.createElement("div");
+            itemRightContainer.className = "shopping-cart-item-right";
+            cartContainer.insertBefore(line, cartEmptyMessage);
+            cartContainer.insertBefore(itemContainer, cartEmptyMessage);
+            itemContainer.appendChild(itemLeftContainer);
+            itemContainer.appendChild(itemRightContainer);
 
-        // content in the left container: image, name
-        let itemImage = document.createElement("div");
-        itemImage.classList.add("shopping-cart-item-img");
-        let itemName = document.createElement("div");
-        itemName.classList.add("underlined");
-        itemLeftContainer.appendChild(itemImage);
-        itemLeftContainer.appendChild(itemName);
+            // content in the left container: image, name
+            let itemImage = document.createElement("div");
+            itemImage.classList.add("shopping-cart-item-img");
+            let itemName = document.createElement("div");
+            itemName.classList.add("underlined");
+            itemLeftContainer.appendChild(itemImage);
+            itemLeftContainer.appendChild(itemName);
 
-        let text = document.createTextNode(name);
-        itemName.appendChild(text);
-        
-        // add item-specific image classes to itemImage
-        
-        switch(name) {
-            case "Original":
-                itemImage.classList.add("shopping-cart-original-image");
-                break;
-            case "Blackberry":
-                itemImage.classList.add("shopping-cart-blackberry-image");
-                break;
-            case "Walnut":
-                itemImage.classList.add("shopping-cart-walnut-image");
-                break;
-            case "Original Gluten Free":
-                itemImage.classList.add("shopping-cart-original-gluten-free-image");
-                break;
-            case "Pumpkin Spice":
-                itemImage.classList.add("shopping-cart-pumpkin-spice-image");
-                break;
-            case "Caramel Peacan":
-                itemImage.classList.add("shopping-cart-caramel-peacan-image");
-                break;
-        
-            default:
-                itemImage.classList.add("shopping-cart-original-image");
-                break;
-        }
-
-        // content in the right container: two dropdowns, price, delete button
-
-        // quantity dropdown
-        let quantityDropdown = document.createElement("select");
-        quantityDropdown.classList.add("cart-dropdown-appearance");
-        itemContainer.appendChild(quantityDropdown);
-        // array of quantity dropdown values
-        let quantityValues = [1, 3, 6, 12];
-        // array of quantity dropdown texts
-        let quantityTexts = ["1 roll", "3 rolls", "6 rolls", "12 rolls"];
-        for(i in quantityValues) {
-            let option = document.createElement("option");
-            option.value = quantityValues[i];
-            option.text = quantityTexts[i];
-            // make sure to display users' previous selection of quantity
-            if (quantity === option.value)
-                option.selected = true;
+            let text = document.createTextNode(name);
+            itemName.appendChild(text);
             
-                quantityDropdown.appendChild(option);
-        }
-
-        // glazing dropdown
-        let glazingDropdown = document.createElement("select");
-        glazingDropdown.classList.add("cart-dropdown-appearance");
-        itemContainer.appendChild(glazingDropdown);
-        // array of glazing dropdown values
-        let glazingValues = ["None", "Sugar Milk", "Vanilla Milk", "Double Chocolate"];
-        // array of glazing dropdown texts
-        let glazingTexts = ["none", "sugar milk", "vanilla milk", "double chocolate"];
-        for(let j = 0; j < glazingValues.length; j++) {
-            let option = document.createElement("option");
-            option.value = glazingValues[j];
-            option.textContent = glazingTexts[j];
-            // make sure to display users' previous selection of glazing
-            if (glazing === option.value)
-                option.selected = true;
+            // add item-specific image classes to itemImage
             
-                glazingDropdown.appendChild(option);
+            switch(name) {
+                case "Original":
+                    itemImage.classList.add("shopping-cart-original-image");
+                    break;
+                case "Blackberry":
+                    itemImage.classList.add("shopping-cart-blackberry-image");
+                    break;
+                case "Walnut":
+                    itemImage.classList.add("shopping-cart-walnut-image");
+                    break;
+                case "Original Gluten Free":
+                    itemImage.classList.add("shopping-cart-original-gluten-free-image");
+                    break;
+                case "Pumpkin Spice":
+                    itemImage.classList.add("shopping-cart-pumpkin-spice-image");
+                    break;
+                case "Caramel Peacan":
+                    itemImage.classList.add("shopping-cart-caramel-peacan-image");
+                    break;
+            
+                default:
+                    itemImage.classList.add("shopping-cart-original-image");
+                    break;
+            }
+
+            // content in the right container: two dropdowns, price, delete button
+
+            // quantity dropdown
+            let quantityDropdown = document.createElement("select");
+            quantityDropdown.classList.add("cart-dropdown-appearance");
+            itemContainer.appendChild(quantityDropdown);
+            // array of quantity dropdown values
+            let quantityValues = [1, 3, 6, 12];
+            // array of quantity dropdown texts
+            let quantityTexts = ["1 roll", "3 rolls", "6 rolls", "12 rolls"];
+            for(i in quantityValues) {
+                let option = document.createElement("option");
+                option.value = quantityValues[i];
+                option.text = quantityTexts[i];
+                // make sure to display users' previous selection of quantity
+                if (quantity === option.value)
+                    option.selected = true;
+                
+                    quantityDropdown.appendChild(option);
+            }
+
+            // glazing dropdown
+            let glazingDropdown = document.createElement("select");
+            glazingDropdown.classList.add("cart-dropdown-appearance");
+            itemContainer.appendChild(glazingDropdown);
+            // array of glazing dropdown values
+            let glazingValues = ["None", "Sugar Milk", "Vanilla Milk", "Double Chocolate"];
+            // array of glazing dropdown texts
+            let glazingTexts = ["none", "sugar milk", "vanilla milk", "double chocolate"];
+            for(let j = 0; j < glazingValues.length; j++) {
+                let option = document.createElement("option");
+                option.value = glazingValues[j];
+                option.textContent = glazingTexts[j];
+                // make sure to display users' previous selection of glazing
+                if (glazing === option.value)
+                    option.selected = true;
+                
+                    glazingDropdown.appendChild(option);
+            }
+
+            // price
+            let priceContainer = document.createElement("div");
+            priceContainer.classList.add("shopping-cart-item-price");
+            let moneySymbol = document.createElement("span");
+            moneySymbol.className = "golden-color";
+            moneySymbol.textContent = "$";
+            // dynamically loading price based on user's choice
+            let priceNum = Number(quantity * 3.00);
+            // update the total amount on page
+            totalAmountNum += priceNum;
+            let priceText = document.createTextNode(" " + String(priceNum.toFixed(2)));
+            priceContainer.appendChild(moneySymbol);
+            priceContainer.appendChild(priceText);
+            itemContainer.appendChild(priceContainer);
+
+            // delete button
+            let deleteButton = document.createElement("button");
+            deleteButton.className = "delete-button golden-icon";
+            deleteButton.addEventListener("click", () => {
+                console.log(items);
+                delete items[key];
+                localStorage.setItem("productItems", JSON.stringify(items));
+                location.reload();
+            });
+            
+            // append them all to the right container
+            itemContainer.appendChild(priceContainer);
+            itemRightContainer.appendChild(quantityDropdown);
+            itemRightContainer.appendChild(glazingDropdown);
+            itemRightContainer.appendChild(priceContainer);
+            itemRightContainer.appendChild(deleteButton);
         }
 
-        // price
-        let priceContainer = document.createElement("div");
-        priceContainer.classList.add("shopping-cart-item-price");
-        let moneySymbol = document.createElement("span");
-        moneySymbol.className = "golden-color";
-        moneySymbol.textContent = "$";
-        // dynamically loading price based on user's choice
-        let priceNum = Number(quantity * 3.00);
-        // update the total amount on page
-        totalAmountNum += priceNum;
-        let priceText = document.createTextNode(" " + String(priceNum.toFixed(2)));
-        priceContainer.appendChild(moneySymbol);
-        priceContainer.appendChild(priceText);
-        itemContainer.appendChild(priceContainer);
-
-        // delete button
-        let deleteButton = document.createElement("button");
-        deleteButton.className = "delete-button golden-icon";
-        deleteButton.addEventListener("click", () => {
-            console.log(items);
-            delete items[key];
-            localStorage.setItem("productItems", JSON.stringify(items));
-            location.reload();
-        });
-        
-        // append them all to the right container
-        itemContainer.appendChild(priceContainer);
-        itemRightContainer.appendChild(quantityDropdown);
-        itemRightContainer.appendChild(glazingDropdown);
-        itemRightContainer.appendChild(priceContainer);
-        itemRightContainer.appendChild(deleteButton);
+        // calculate the total amount
+        let amountNode = document.createTextNode(String(totalAmountNum.toFixed(2)));
+        totalAmount.appendChild(amountNode);
+        totalAmount.style.display = "inline-block";
+        checkoutButton.style.display = "inline-block";
     }
 
-    // calculate the total amount
-    let amountNode = document.createTextNode(String(totalAmountNum.toFixed(2)));
-    totalAmount.appendChild(amountNode);
-    totalAmount.style.display = "inline-block";
-    checkoutButton.style.display = "inline-block";
+    // if the localstorage object exists, but is empty
+    else {
+        document.getElementsByClassName("cart-empty-message")[0].style.display = "block";
+    }
+
 }
 
-// if the cart is empty
+
+// if the localstorage obejct doesn't exist, meaning cart is empty
 else {
     document.getElementsByClassName("cart-empty-message")[0].style.display = "block";
 }
