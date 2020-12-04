86fa648ac4f015c271ccd8cc45e62fd664ea5920
Yuwen <wayne.lu1998@gmail.com>
2020-12-04 13:43:42 -0500

    Initial commit for Assignment 8

---
+++ assignment_8/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Hello fellas <a href="google.com">Hello</a> hello hello
+      </p>
+    </div>
+  );
+}
+
+export default App;
