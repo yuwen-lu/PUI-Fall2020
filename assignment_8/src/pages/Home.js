1e397f210cbdec8bd4f6e86895fe16781cc9b4ec
Yuwen <wayne.lu1998@gmail.com>
2020-12-09 01:03:31 -0500

    Add AOS library

--- assignment_8/src/pages/Home.js
+++ assignment_8/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 import '../App.css';
@@ -8,14 +10,22 @@ import ProfilePic from '../resources/images/personal-profile.jpg';
 // Functional Component for homepage
 
 export default function Home() {
+    // the animate on scroll library to animate items on home page
+    AOS.init();
+
     return (
         <React.Fragment>
             <Header />
             <div className="content-wrapper">
-                <section id="introduction" style={sectionStyle}>
+                <section 
+                    id="introduction" 
+                    style={sectionStyle} 
+                    data-aos="fade-in" 
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in-out">
                     <div className="text-column" style={introductionStyle}>
                         <p className="title">Hi there! I'm Yuwen.</p>
-                        <p>I am a <a href={"https://www.hcii.cmu.edu/academics/mhci"}>Master of Human-Computer Interaction</a> student at Carnegie Mellon University, School of Computer Science.</p>
+                        <p>I am a <a target="_blank" href={"https://www.hcii.cmu.edu/academics/mhci"}>Master of Human-Computer Interaction</a> student at Carnegie Mellon University, School of Computer Science.</p>
                         <p>I'm an aspired Social Media researcher and UX practitioner.</p>
                         <p>Jump to my <Link to="/resume">Resume</Link>.</p>
                     </div>
@@ -23,7 +33,12 @@ export default function Home() {
                         <img className="profile-pic" style={profilePicStyle} src={ProfilePic} alt="Me"></img>
                     </div>
                 </section>
-                <section id="experience" style={sectionStyle}>
+                <section 
+                    id="experience" 
+                    style={sectionStyle} 
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in-out">
                     <div id="skill-cloud" style={iconCloudStyle}>
                         Hello
                     </div>
@@ -33,7 +48,12 @@ export default function Home() {
                         </p>
                     </div>
                 </section>
-                <section id="interest" style={sectionStyle}>
+                <section 
+                    id="interest" 
+                    style={sectionStyle} 
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in-out">
                     <div className="text-column" style={experienceTextStyle}>
                         My research interest is broadly in <strong>Social Computing</strong>. I seize to use mixed method research to <strong>understand people’s behavior</strong> at scale on social media platforms, and use UX design principles and programming to <strong>improve existing platforms</strong> and promote pro-social outcomes.
                     </div>
--- assignment_8/src/pages/Projects.js
+++ assignment_8/src/pages/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
 import ProjectCard from '../layout/ProjectCard';
@@ -9,6 +11,8 @@ import  HitRanger from './HitRanger';
 // Functional Component for the project page
 
 export default function Projects() {
+    AOS.init();
+
     return (
         <React.Fragment>
             <Header />
@@ -16,20 +20,79 @@ export default function Projects() {
                 {/* the p tag needs a container, otherwise the width of p tag would be the same as the parent tag */}
                 <div className="title-wrapper" style={titleWrapperStyle}><p className="title" style={projectTitleStyle}>Projects</p></div>
                 <div className="project-card-wrapper" style={projectCardWrapperStyle}>
-                    <Link className="project-card-link" to="/ HitRanger">
-                        <ProjectCard title="Pre/Post COVID-19 Twitter Work-related Discussion Analysis" description="Quantitative analysis on Work-related discussions on Twitter before/after COVID-19" job="Quantitative Analysis Researcher" time= "Target Venue: PLoS One 2021" bgImage="/images/twitter-work-related-discussion.jpg"/>
+                    <Link 
+                        className="project-card-link" 
+                        to="/ HitRanger" 
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
+                        <ProjectCard 
+                            title="Pre/Post COVID-19 Twitter Work-related Discussion Analysis" 
+                            description="Quantitative analysis on Work-related discussions on Twitter before/after COVID-19" 
+                            job="Quantitative Analysis Researcher" 
+                            time= "Target Venue: PLoS One 2021" 
+                            bgImage="/images/twitter-work-related-discussion.jpg"
+                        />
                     </Link>
-                    <Link className="project-card-link" to="/ HitRanger">
-                        <ProjectCard title="COVID-19 Twitter Mask Analysis" description="Qualitative analysis on Mask discussions on Twitter through the lens of Risk Perception" job="Qualitative Analysis Researcher" time= "Target Venue: PLoS One 2021" bgImage="/images/twitter-mask-analysis.jpg"/>
+                    <Link 
+                        className="project-card-link" 
+                        to="/ HitRanger"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out"
+                        >
+                        <ProjectCard 
+                            title="COVID-19 Twitter Mask Analysis" 
+                            description="Qualitative analysis on Mask discussions on Twitter through the lens of Risk Perception" 
+                            job="Qualitative Analysis Researcher" 
+                            time= "Target Venue: PLoS One 2021" 
+                            bgImage="/images/twitter-mask-analysis.jpg"
+                        />
                     </Link>
-                    <Link className="project-card-link" to="/ HitRanger">
-                        <ProjectCard title="Goalplay Video Coach" description="Personal iOS training APP for amateur goalkeepers with Augmented Reality and Machine Learning" job="iOS Developer, Usability Manager" time= "2019" bgImage="/images/hit-ranger-icon.png"/>
+                    <Link 
+                        className="project-card-link" 
+                        to="/ HitRanger"
+                        data-aos="fade-up"
+                        data-aos-offset="200"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
+                        <ProjectCard 
+                        title="Goalplay Video Coach" 
+                        description="Personal iOS training APP for amateur goalkeepers with Augmented Reality and Machine Learning" 
+                        job="iOS Developer, Usability Manager" 
+                        time= "2019" 
+                        bgImage="/images/hit-ranger-icon.png"
+                        />
                     </Link>
-                    <Link className="project-card-link" to="/ HitRanger">
-                        <ProjectCard title="HIT Ranger" description="Mobilizing Crowdwork with Task Management Interfaces" job="Developer" time= "Target Venue: CSCW 2021" bgImage="/images/goalplay-screenshot-homepage-lowres.png"/>
+                    <Link 
+                        className="project-card-link" 
+                        to="/ HitRanger"
+                        data-aos="fade-up"
+                        data-aos-offset="200"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
+                        <ProjectCard 
+                            title="HIT Ranger" 
+                            description="Mobilizing Crowdwork with Task Management Interfaces" 
+                            job="Developer" 
+                            time= "Target Venue: CSCW 2021" 
+                            bgImage="/images/goalplay-screenshot-homepage-lowres.png"
+                        />
                     </Link>
-                    <Link className="project-card-link" to="/ HitRanger">
-                        <ProjectCard title="Embeded Interface Design" description="Designing YouTube advertisement content to promote pro-social behaviors online" job="Researcher" time= "2020" bgImage="/images/youtube-embeded-interface-design.png"/>
+                    <Link 
+                        className="project-card-link" 
+                        to="/ HitRanger"
+                        data-aos="fade-up"
+                        data-aos-offset="200"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease-in-out">
+                        <ProjectCard 
+                            title="Embeded Interface Design" 
+                            description="Designing YouTube advertisement content to promote pro-social behaviors online" 
+                            job="Researcher" 
+                            time= "2020" 
+                            bgImage="/images/youtube-embeded-interface-design.png"
+                        />
                     </Link>
                 </div>
             </div>
