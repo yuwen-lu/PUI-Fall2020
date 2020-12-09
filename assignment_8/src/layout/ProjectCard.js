07fc6b904148ff7c4b1c81ca5ad2098622013423
Yuwen <wayne.lu1998@gmail.com>
2020-12-08 23:30:49 -0500

    Finish project card styling

--- assignment_8/src/layout/ProjectCard.js
+++ assignment_8/src/layout/ProjectCard.js
@@ -1,20 +1,72 @@
 import React from 'react';
 
-export default function ProjectCard () {
+export default function ProjectCard (props) {
+    console.log(props.bgImage);
 
     return (
+        
         <div className="project-card" style={projectCardStyle}>
-            Project Card
+            <div style={{...projectCardBgImgStyle, backgroundImage: "url(" + props.bgImage +")"}}></div>
+            <div style={projectCardTextContainerStyle}>
+                <div style={cardTitleStyle}>{props.title}</div>
+                <div style={cardDescriptionStyle}>{props.description}</div>
+                <div>
+                    <div style={cardDetailStyle}>{props.job}</div>
+                    <div style={cardDetailStyle}>{props.time}</div>
+                </div>
+            </div>
         </div>
     )
 }
 
+// the following three style sheet variables are for project cards.
+// since it's hard to create psuedo elements in react, I added a separate div for the whole card, a div for the background image, and a div for the text.
+
 const projectCardStyle = {
-    display: "inline-block",
-    width: "22rem",
-    height: "28vh",
-    border: "#fff 3px solid",
+    width: "25rem",
+    height: "18rem",
     borderRadius: "1rem",
     margin: "2rem auto",
+    // margin: "2rem auto",
+    
+}
+
+const projectCardBgImgStyle = {
+    width: "25rem",
+    height: "18rem",
+    borderRadius: "1rem",
+    backgroundPosition: "center", /* Center the image */
+    backgroundRepeat: "no-repeat", /* Do not repeat the image */
+    backgroundSize: "cover", /* Resize the background image to cover the entire container */
+    opacity: "15%"
+}
+
+const projectCardTextContainerStyle = {
+    width: "25rem",
+    height: "18rem",
     padding: "1rem 1.5rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    position: "relative",
+    bottom: "18rem"
+}
+
+const cardTitleStyle = {
+    hyphen: "none",
+    fontSize: "1rem",
+    lineHeight: "1.5rem",
+    marginBottom: "1.5rem"
+
+}
+
+const cardDescriptionStyle = {
+    fontWeight: "500",
+    lineHeight: "2rem",
+}
+
+const cardDetailStyle = {
+    fontSize: "0.8rem",
+    lineHeight: "1.5rem",
+    textAlign: "right"
 }
--- assignment_8/src/pages/Projects.js
+++ assignment_8/src/pages/Projects.js
@@ -14,11 +14,11 @@ export default function Projects() {
                 {/* the p tag needs a container, otherwise the width of p tag would be the same as the parent tag */}
                 <div className="title-wrapper" style={titleWrapperStyle}><p className="title" style={projectTitleStyle}>Projects</p></div>
                 <div className="project-card-wrapper" style={projectCardWrapperStyle}>
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
+                    <ProjectCard title="Pre/Post COVID-19 Twitter Work-related Discussion Analysis" description="Quantitative analysis on Work-related discussions on Twitter before/after COVID-19" job="Quantitative Analysis Researcher" time= "Target Venue: PLoS One 2021" bgImage="/images/twitter-work-related-discussion.jpg"/>
+                    <ProjectCard title="COVID-19 Twitter Mask Analysis" description="Qualitative analysis on Mask discussions on Twitter through the lens of Risk Perception" job="Qualitative Analysis Researcher" time= "Target Venue: PLoS One 2021" bgImage="/images/twitter-mask-analysis.jpg"/>
+                    <ProjectCard title="Goalplay Video Coach" description="Personal iOS training APP for amateur goalkeepers with Augmented Reality and Machine Learning" job="iOS Developer, Usability Manager" time= "2019" bgImage="/images/hit-ranger-icon.png"/>
+                    <ProjectCard title="HIT Ranger" description="Mobilizing Crowdwork with Task Management Interfaces" job="Developer" time= "Target Venue: CSCW 2021" bgImage="/images/goalplay-screenshot-homepage-lowres.png"/>
+                    <ProjectCard title="Embeded Interface Design" description="Designing YouTube advertisement content to promote pro-social behaviors online" job="Researcher" time= "2020" bgImage="/images/youtube-embeded-interface-design.png"/>
                 </div>
             </div>
             <Footer />
@@ -27,14 +27,12 @@ export default function Projects() {
 }
 
 const projectCardWrapperStyle = {
-    // display: "flex",
-    // flexDirection: "row",
-    // flexWrap: "wrap",
-    // justifyContent: "space-evenly",
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, 20rem)",
-    gridGap: "2rem",
-    justifyContent: "space-between"
+    width: "60rem",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly"
 
 }
 
